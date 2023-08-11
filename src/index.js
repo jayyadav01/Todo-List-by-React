8a22e23420f5ed3db61721bcfe0abed60667a631
jayyadav01 <jayyadav.rbt@gmail.com>
2023-08-11 13:25:10 +0530

    Todo List

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import TodoList from './TodoList';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <TodoList />
